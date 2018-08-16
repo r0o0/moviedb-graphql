c9a0ab9a496275a743517db4c88e74fdad6c1430
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-08-16 17:27:26 +0900

    add movieSearch fetch by keyword from TMDb API

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -5,13 +5,14 @@ dotenv.config();
 const key = process.env.API_KEY;
 const configs_url = `https://api.themoviedb.org/3/configuration?api_key=${key}`;
 const api_url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}`;
+const search_url = `https://api.themoviedb.org/3/search/movie/?api_key=${key}`
 
-export const getConfigs = () =>
+const getConfigs = () =>
   fetch(configs_url)
     .then(res => res.json())
     .then(configs => configs.images);
 
-export const getNowPlaying = (parent, page, lang) => {
+const getNowPlaying = (parent, page, lang) => {
   // region
   const region = lang.substr(3, 4);
   // poster sizes
@@ -42,7 +43,7 @@ export const getNowPlaying = (parent, page, lang) => {
     });
 }
 
-export const getMovies = parent => {
+const getMovies = parent => {
   const movies = [];
   const data = parent.movies;
 
@@ -60,6 +61,40 @@ export const getMovies = parent => {
     }
     movies.push(results);
   });
-  
   return movies;
 };
+
+const getSearchResult = (parent, keyword, lang) => {
+  // region
+  const region = lang.substr(3, 4);
+  // poster sizes
+  const sizes = parent.poster_sizes;
+  const size_original = sizes[sizes.length - 1];
+  const size_large = sizes[sizes.length - 2];
+  const size_medium = sizes[sizes.length - 3];
+  const size_small = sizes[sizes.length - 4];
+
+  return fetch(`${search_url}&query=${keyword}&language=${lang}`)
+    .then(res => res.json())
+    .then(data => {
+      const results = {
+        type: "search",
+        keyword: keyword,
+        page : data.page,
+        total : data.total_results,
+        total_pages : data.total_pages,
+        date : data.dates,
+        image_url : parent.secure_base_url,
+        image_original : size_original,
+        image_large : size_large,
+        image_medium : size_medium,
+        image_small : size_small,
+        movies : data.results,
+      }
+      return results;
+    });
+}
+
+
+
+export { getConfigs, getNowPlaying, getMovies, getSearchResult };
